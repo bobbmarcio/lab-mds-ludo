90394eccd408ef9f1c73452c6957b16f1cdbfdc0
Márcio Flores <marciogabriel07@gmail.com>
2017-12-03 19:25:05 -0200

    Initial commit

---
+++ src/Modelos/Jogador.java
@@ -0,0 +1,21 @@
+package Modelos;
+
+public class Jogador {
+    public String nome;
+
+    public Jogador(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void jogarDado(){
+
+    }
+
+    public void moverPeca(){
+
+    }
+}
