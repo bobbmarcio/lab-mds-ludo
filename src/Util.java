9f9d07d4ed4b0575d47cf36c98c39ddb1e47182f
Márcio Flores <marciogabriel07@gmail.com>
2017-12-06 16:22:39 -0200

    Arquivo Jar

--- src/Util.java
+++ src/Util.java
@@ -1,9 +1,11 @@
+import java.util.List;
+
 public class Util {
 
     public static final int DENTRO_DA_BASE = 0;
     public static final int FORA_DA_BASE = 1;
 
-    public void valida(){
-
+    public void valida(List<Jogador> jogadores, int i){
+        jogadores.get(i).getPeca().getPosicao();
     }
 }
